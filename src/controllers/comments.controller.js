17eb402f7ba01b147a19217256dbf7166ad56eb5
Daniel Freudberg <dbfreudber@gmail.com>
2018-07-11 14:14:44 -0400

    Fixed a arrow function, the other one, however, refuses to work if I change it to ES6.

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -9,15 +9,20 @@ class CommentsController {
 
   addCommentFormListener() {
     var _this = this
-    // With my 2 months since my class was over I needed to google here a bunch. We didn't go over jquery in my course but it was easier to do here than vanilla.
-    this.$addCommentForm.each(function(index) {
-      $(this).on('submit', function(e) {
+    // With my 2 months since my class was over I needed to google here a bunch. We didn't go over jquery in my course but it seemed easier to do here than vanilla.
+    this.$addCommentForm.each(function(
+      index // Not sure why but if I make this an arrow function it breaks.
+    ) {
+      $(this).on('submit', e => {
         e.preventDefault()
         let $imageId = $(`#image-${index}`).data('id')
         let $comment = $(`#comment-description-${index}`).val()
+        //prevent blank comments
         if ($comment !== '') {
           let newComment = new Comment($comment, $imageId)
           _this.render(newComment)
+        } else {
+          console.log('this is empty')
         }
       })
     })
